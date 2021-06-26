86604e928f1fc8bddf3e79439367f179703760ad
shubhamjain31 <sj27754@gmail.com>
2021-06-26 21:37:01 +0530

    Simple Contact Book Structure

---
+++ contact_book/main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+
+from views import Window
+
+def main():
+
+    """ main function."""
+
+    # Create the application
+    app = QApplication(sys.argv)
+
+    # Create the main window
+    win = Window()
+    win.show()
+    
+    # Run the event loop
+    sys.exit(app.exec())
---
+++ contact_book/views.py
@@ -0,0 +1,41 @@
+from PyQt5.QtWidgets import (
+    QAbstractItemView,
+    QHBoxLayout,
+    QMainWindow,
+    QPushButton,
+    QTableView,
+    QVBoxLayout,
+    QWidget,
+)
+
+class Window(QMainWindow):
+    """Main Window."""
+    def __init__(self, parent=None):
+        """Initializer."""
+        # Snip...
+
+        self.setupUI()
+
+    def setupUI(self):
+
+        """Setup the main window's GUI."""
+
+        # Create the table view widget
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+
+        # Create buttons
+        self.addButton = QPushButton("Add...")
+        self.deleteButton = QPushButton("Delete")
+        self.clearAllButton = QPushButton("Clear All")
+
+        # Lay out the GUI
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
